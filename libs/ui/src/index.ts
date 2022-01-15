b52e35d14241078ae2f3a52372ce69b12a67a0b7
msanvarov <msalanvarov@gmail.com>
2022-01-15 17:18:23 -0500

    :art: exporting created components

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -4,6 +4,11 @@ export * from './lib/loader.component';
 export * from './lib/avatar.component';
 export * from './lib/toast.component';
 export * from './lib/header.component';
+export * from './lib/page-heading.component';
+export * from './lib/app-layout.component';
+export * from './lib/input.component';
+export * from './lib/link.component';
+export * from './lib/footer.component';
 
 // folder modules
 export * from './lib/navbar';
