47fde776aab39fdee482d0e747ae6d08d3d30b60
msanvarov <msalanvarov@gmail.com>
2022-01-15 23:40:50 -0500

    :art: fixing z index on the toast

--- libs/ui/src/lib/toast.component.tsx
+++ libs/ui/src/lib/toast.component.tsx
@@ -51,7 +51,7 @@ export const Toast: React.FC<ToastProps> = ({
   return (
     <div
       aria-live="assertive"
-      className="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start"
+      className="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start z-50"
     >
       <div className="w-full flex flex-col items-center space-y-4 sm:items-end">
         <Transition
