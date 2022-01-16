5e868a35fd5a09ea56e8ba1fe6cffd7f98de3a4f
msanvarov <msalanvarov@gmail.com>
2022-01-16 04:43:54 -0500

    :sparkles: method to create username and link the username to user

--- libs/redux/src/lib/firebase/firestore-queries.ts
+++ libs/redux/src/lib/firebase/firestore-queries.ts
@@ -1,8 +1,30 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, writeBatch } from 'firebase/firestore';
 
 import { db } from './index';
 
 export const checkUsername = async (username: string) => {
   const usernameDoc = await getDoc(doc(db, 'users', username));
   return usernameDoc.exists();
+};
+
+export const createUsernameAndLinkToUser = async ({
+  uid,
+  username,
+  displayName,
+  photoURL,
+}: {
+  uid: string;
+  username: string;
+  photoURL?: string;
+  displayName?: string;
+}) => {
+  // displayName can be the users name when they sign up with Google. Or it can be their username.
+  const batch = writeBatch(db);
+  batch.set(doc(db, 'users', uid), {
+    username,
+    photoURL: photoURL,
+    displayName: displayName || username,
+  });
+  batch.set(doc(db, 'usernames', username), { uid });
+  return await batch.commit();
 };
