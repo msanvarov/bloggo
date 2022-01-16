e6bbaceb8f48a7cde313f54a8cb61b348f3db4c4
msanvarov <msalanvarov@gmail.com>
2022-01-15 23:40:04 -0500

    :hammer: universal tailwind workspace preset

---
+++ tailwind-workspace-preset.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
